aaeedb30fe42a9e5dea03478fc284b36c5a22e06
Nicholas Casale <nicholas.e.casale@gmail.com>
2023-03-14 16:12:09 -0500

    Arrays and objects

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,4 +1,12 @@
 const apples: number = 5;
 let speed: string = "fast";
 let hasName: boolean = true;
-let nothingMuch: null = null; 
+let nothingMuch: null = null;
+
+// built in object
+let now: Date = new Date();
+
+//Array 
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
